61875cb0781c46f765695cfa0e6df9e271cf9735
Imran Jabrayilov <c.imran2001@yandex.com>
2022-10-11 00:33:23 +0400

    Homework1 added

---
+++ Homework1_Tests/UnitTest1.cs
@@ -0,0 +1,67 @@
+using Homework_Chapter_3_New;
+
+namespace Homework1_Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestSum()
+        {
+            var fraction1 = new Fraction(1, 2);
+            var fraction2 = new Fraction(4, 5);
+
+            var result = fraction1 + fraction2;
+            Assert.Equal("13/10", result.ToString());
+        }
+
+        [Fact]
+        public void TestDiff()
+        {
+            var fraction1 = new Fraction(10, 15);
+            var fraction2 = new Fraction(10, 30);
+
+            var result = fraction1 - fraction2;
+
+            Assert.Equal("1/3", result.ToString());
+        }
+
+        [Fact]
+        public void TestProduct()
+        {
+            var fraction1 = new Fraction(3, 7);
+            var fraction2 = new Fraction(7, 12);
+
+            var result = fraction1 * fraction2;
+            Assert.Equal("1/4", result.ToString());
+        }
+
+        [Fact]
+        public void TestDivision()
+        {
+            var fraction1 = new Fraction(5, 2);
+            var fraction2 = new Fraction(5, 6);
+
+            var result = fraction1 / fraction2;
+            Assert.Equal("3/1", result.ToString());
+        }
+
+        [Fact]
+        public void TestException()
+        {
+            var fraction1 = new Fraction(1, 2);
+            var fraction2 = new Fraction(0, 3);
+
+            var exception = Assert.Throws<InvalidOperationException>(() => fraction1 / fraction2);
+            Assert.Equal("Denominator can't be zero.", exception.Message);
+        }
+
+        [Fact]
+        public void TestValue()
+        {
+            var fraction1 = new Fraction(1, 2);
+            var fraction2 = new Fraction(4, 1);
+            var result = fraction1 * fraction2;
+            Assert.Equal(2.0, result.Value);
+        }
+    }
+}
